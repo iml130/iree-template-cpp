44d9d051c193f1757ac7a2aa478f0459de10cc16
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2020-04-12 17:44:57 +0200

    Import and adapt custom_modules dialect sources

---
+++ custom_modules/dialect/conversion_patterns.cc
@@ -0,0 +1,61 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Forked from IREE with modified includes
+
+#include "conversion_patterns.h"
+
+#include "iree/compiler/Dialect/HAL/Conversion/ConversionTarget.h"
+#include "iree/compiler/Dialect/VM/Conversion/ImportUtils.h"
+#include "custom_dialect.h"
+
+namespace mlir {
+namespace iree_compiler {
+namespace IREE {
+namespace Custom {
+
+void populateCustomToHALPatterns(MLIRContext *context,
+                                 OwningRewritePatternList &patterns,
+                                 TypeConverter &typeConverter) {
+  // We can use the HAL conversion handler for this tensor->buffer conversion
+  // as we just want the simple form. If we wanted to perform additional
+  // verification or have a specific use case (such as a place where only the
+  // buffer is required and the shape is not) we could add our own.
+  patterns.insert<HALOpConversion<TensorToMessageOp, BufferToMessageOp>>(
+      context, typeConverter);
+  patterns.insert<HALOpConversion<MessageToTensorOp, MessageToBufferOp>>(
+      context, typeConverter);
+}
+
+void populateCustomToVMPatterns(MLIRContext *context,
+                                SymbolTable &importSymbols,
+                                OwningRewritePatternList &patterns,
+                                TypeConverter &typeConverter) {
+  // We can use the VM conversion handler for all of these as they are simple
+  // 1:1 mappings. More complex mappings can provide their own conversions
+  // (such as the HAL dialect does).
+  patterns.insert<VMImportOpConversion<IREE::Custom::BufferToMessageOp>>(
+      context, importSymbols, typeConverter, "custom.buffer_to_message");
+  patterns.insert<VMImportOpConversion<IREE::Custom::MessageToBufferOp>>(
+      context, importSymbols, typeConverter, "custom.message_to_buffer");
+  patterns.insert<VMImportOpConversion<IREE::Custom::PrintOp>>(
+      context, importSymbols, typeConverter, "custom.print");
+  patterns.insert<VMImportOpConversion<IREE::Custom::ReverseOp>>(
+      context, importSymbols, typeConverter, "custom.reverse");
+  patterns.insert<VMImportOpConversion<IREE::Custom::GetUniqueMessageOp>>(
+      context, importSymbols, typeConverter, "custom.get_unique_message");
+}
+
+}  // namespace Custom
+}  // namespace IREE
+}  // namespace iree_compiler
+}  // namespace mlir
---
+++ custom_modules/dialect/custom_dialect.cc
@@ -0,0 +1,113 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Forked from IREE with modified includes
+
+#include "custom_dialect.h"
+#include "conversion_patterns.h"
+#include "custom.imports.h"
+
+#include "iree/compiler/Dialect/HAL/Conversion/ConversionDialectInterface.h"
+#include "iree/compiler/Dialect/VM/Conversion/ConversionDialectInterface.h"
+#include "llvm/ADT/StringSwitch.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/SymbolTable.h"
+#include "mlir/Parser.h"
+#include "mlir/Transforms/DialectConversion.h"
+
+namespace mlir {
+namespace iree_compiler {
+namespace IREE {
+namespace Custom {
+
+namespace {
+
+static DialectRegistration<CustomDialect> custom_dialect;
+
+// Exposes conversion patterns that transition tensors to buffers during the
+// Flow->HAL dialect lowering. This is only required if the dialect has ops that
+// use tensor types.
+class CustomToHALConversionInterface : public HALConversionDialectInterface {
+ public:
+  using HALConversionDialectInterface::HALConversionDialectInterface;
+
+  void setupConversionTarget(ConversionTarget &target,
+                             OwningRewritePatternList &patterns,
+                             TypeConverter &typeConverter) const override {
+    populateCustomToHALPatterns(getDialect()->getContext(), patterns,
+                                typeConverter);
+  }
+};
+
+// Exposes the import module and conversion patterns used to convert custom
+// ops to their vm.import counterparts.
+class CustomToVMConversionInterface : public VMConversionDialectInterface {
+ public:
+  using VMConversionDialectInterface::VMConversionDialectInterface;
+
+  OwningModuleRef getVMImportModule() const override {
+    return mlir::parseSourceString(
+        StringRef(custom_imports_create()->data, custom_imports_create()->size),
+        getDialect()->getContext());
+  }
+
+  void populateVMConversionPatterns(
+      SymbolTable &importSymbols, OwningRewritePatternList &patterns,
+      TypeConverter &typeConverter) const override {
+    populateCustomToVMPatterns(getDialect()->getContext(), importSymbols,
+                               patterns, typeConverter);
+  }
+};
+
+}  // namespace
+
+CustomDialect::CustomDialect(MLIRContext *context)
+    : Dialect(getDialectNamespace(), context) {
+  addInterfaces<CustomToHALConversionInterface,
+                CustomToVMConversionInterface>();
+
+  addTypes<MessageType>();
+
+#define GET_OP_LIST
+  addOperations<
+#include "custom_ops.cc.inc"
+      >();
+}
+
+Type CustomDialect::parseType(DialectAsmParser &parser) const {
+  StringRef typeName;
+  if (failed(parser.parseKeyword(&typeName))) return {};
+  auto type = llvm::StringSwitch<Type>(typeName)
+                  .Case("message", MessageType::get(getContext()))
+                  .Default(nullptr);
+  if (!type) {
+    parser.emitError(parser.getCurrentLocation())
+        << "unknown type: " << typeName;
+  }
+  return type;
+}
+
+void CustomDialect::printType(Type type, DialectAsmPrinter &p) const {
+  if (type.isa<MessageType>()) {
+    p << "message";
+  } else {
+    llvm_unreachable("unknown type");
+  }
+}
+
+#define GET_OP_CLASSES
+#include "custom_ops.cc.inc"
+
+}  // namespace Custom
+}  // namespace IREE
+}  // namespace iree_compiler
+}  // namespace mlir
